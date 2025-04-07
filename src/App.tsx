96b8225f187821e857e3b69d09ddc5cc377c7f7b
DawidBrozyna <dawid.brozyna@studenci.collegiumwitelona.pl>
2025-04-07 19:29:52 +0200

    nav bar
    nav bar

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './navbar'
+
+function App() {
+  return <Navbar/>
+}
+
+export default App
---
+++ src/navbar.tsx
@@ -0,0 +1,23 @@
+export default function Navbar(){
+
+    return <nav className="nav">
+        <a className="bimbrownik-logo" href="/"><img></img>Bimbrownik</a>
+        <ul>
+
+            <li>
+                <a className="bimbrownik-buttons" href="/Przepisy">Przepisy</a>
+            </li>
+
+            <li>
+                <a className="bimbrownik-buttons" href="/KącikKonesera">Kącik Konesera</a>
+            </li>
+
+            <li>
+                <a className="bimbrownik-buttons" href="/Logowanie">Logowanie</a>
+            </li>
+        </ul>
+
+
+    </nav>
+
+}
